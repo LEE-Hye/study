26d9a16656f254d5533a6182e1f1e2ac46f88ab1
LEE-Hye <leebin9295@naver.com>
2022-11-07 10:34:00 +0900

    gallery

---
+++ Quest03/skeleton/gallery-start/main.js
@@ -0,0 +1,18 @@
+const displayedImage = document.querySelector('.displayed-img');
+const thumbBar = document.querySelector('.thumb-bar');
+
+const btn = document.querySelector('button');
+const overlay = document.querySelector('.overlay');
+
+/* Declaring the array of image filenames 이미지 파일 이름 배열 선언 */
+
+/* Declaring the alternative text for each image file 각 이미지 파일에 대한 대체 텍스트 선언 */
+
+/* Looping through images 이미지 순회 */
+
+const newImage = document.createElement('img');
+newImage.setAttribute('src', xxx);
+newImage.setAttribute('alt', xxx);
+thumbBar.appendChild(newImage);
+
+/* Wiring up the Darken/Lighten button 어둡게/밝게 버튼 연결 */
