93bf6a188bd91f7020b00cf866eaeab430036cfb
LEE-Hye <leebin9295@naver.com>
2022-11-08 17:28:29 +0900

    getattribute=getattribute가 안된대

--- Quest03/skeleton/gallery-start/main.js
+++ Quest03/skeleton/gallery-start/main.js
@@ -1,10 +1,8 @@
+// getElementsByClassName는 유사 배열인 htmlcollection가 배열 요소를 갖고 오기 때문에 인덱스 0을 붙여야 한다.
 var displayedImage = document.getElementsByClassName('displayed-img')[0];
 var thumbBar = document.getElementsByClassName('thumb-bar')[0];
-console.log('타입 확인 : ' + typeof thumbBar);
-// getElementsByClassName는 객체 타입이라 사용하려면 key값을 뽑아내야 함
 
 var thumbBar_img = thumbBar.getElementsByTagName('img');
-
 var btn = document.getElementsByTagName('button')[0];
 var overlay = document.getElementsByClassName('overlay')[0];
 
@@ -25,22 +23,18 @@ for (i = 1; i <= 5; i++) {
 console.log('이미지 파일 이름 추가됐는지 확인: ' + img_alt);
 
 /* Looping through images 이미지 순회 */
-
 for (i = 0; i < 5; i++) {
     var newImage = document.createElement('img');
     newImage.setAttribute('src', img_src[i]);
     newImage.setAttribute('alt', img_alt[i]);
     thumbBar.appendChild(newImage);
 }
-/* onclick 핸들러를 각 섬네일 이미지에 추가하기*/
 
+/* onclick 핸들러를 각 섬네일 이미지에 추가하기*/
 for (i = 0; i < thumbBar_img.length; i++) {
     (function(i){
     thumbBar_img[i].onclick = function () {
-
         displayedImage.setAttribute('src',thumbBar_img[i].getAttribute('src'))
-        // displayedImage.getAttribute('src') = thumbBar_img[i].getAttribute('src');
-        // displayedImage.getAttribute('alt') = thumbBar_img[i].getAttribute('alt');
     }
 })(i);
 }
@@ -56,12 +50,12 @@ btn.addEventListener('click', function(){
         btn.setAttribute('class', 'light');
         btn.textContent = 'Lighten';
         overlay.style.backgroundColor = 'rgba(0,0,0,0.5)';
-        console.log(btn.getAttribute('class'))
+        // console.log(btn.getAttribute('class'))
     }else{
         btn.setAttribute('class', 'dark');
         btn.textContent = 'Darken';
         overlay.style.backgroundColor = 'rgba(0,0,0,0)';
-       
+        // console.log(btn.getAttribute('class'))
     }
 })
 
